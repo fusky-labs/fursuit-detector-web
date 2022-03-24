29edf17890fe68c9f5eb8697cb6bab510ea34d07
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-25 07:59:32 +0800

    some minor UI stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.scss';
+import Footer from './Footer';
 import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <form>
-        <input type="file" name="image" />
-        <input type="submit" value="Generate results" name="submit" />
-      </form>
+    <div className='app'>
+    <Header/>
+    <main>
+      <div id="container">
+        <form>
+          <input type="file" name="image" />
+          <input type="submit" value="Generate results" name="submit" />
+        </form>
+      </div>
+    </main>
+    <Footer />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return(
     <footer>
-      
+      footer content
     </footer>
   )
 }
