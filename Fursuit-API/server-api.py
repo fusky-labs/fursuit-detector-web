6b3d38ef731bdc63cb4f16956d419bbb9545cf79
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-24 21:25:57 +0800

    Squashing small commits
    Remove tests and got sass working
    
    Update LICENSE
    
    Update README

--- Fursuit-API/server-api.py
+++ Fursuit-API/server-api.py
@@ -8,22 +8,22 @@ cors_allowed = True
 app = Flask(__name__)
 
 if cors_allowed:
-    from flask_cors import CORS
-    CORS(app)
+  from flask_cors import CORS
+  CORS(app)
 
 @app.route('/detect', methods=['POST'])
 def detect():
     if request.method == 'POST':
-        session = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-        image = request.files.get('image')
-        image = np.asarray(bytearray(image.read()), dtype="uint8")
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        print(image.shape)
-        cv2.imwrite(session + '.jpg', image)
-        return jsonify({
-            "status":"success",
-            "session": session
-            })
+      session = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+      image = request.files.get('image')
+      image = np.asarray(bytearray(image.read()), dtype="uint8")
+      image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+      print(image.shape)
+      cv2.imwrite(session + '.jpg', image)
+      return jsonify({
+          "status":"success",
+          "session": session
+          })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
