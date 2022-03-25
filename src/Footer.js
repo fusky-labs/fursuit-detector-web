1feff9aee280abb6d3eb3a18bdd3b4cd5125dd36
thatITfox <maxthecomputerfox@gmail.com>
2022-03-25 10:49:27 +0700

    remanage components managements

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return(
-    <footer>
-      footer content
-    </footer>
-  )
-}
-
-export default Footer;
--- src/Header.js
+++
@@ -1,7 +0,0 @@
-function Header() {
-  return(
-    <h1>Da YOLOv4 Fursuit Detector</h1>
-  )
-}
-
-export default Header;
